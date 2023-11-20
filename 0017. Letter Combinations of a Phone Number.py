8cdf02d77c6271ed82a0c8cbdde4c1a359d90126
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2023-11-20 20:51:11 +0900

    solve 17. Letter Combinations of a Phone Number

--- 0017. Letter Combinations of a Phone Number.py
+++ 0017. Letter Combinations of a Phone Number.py
@@ -10,9 +10,6 @@ class Solution:
 
         A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
-
-        
-
         Example 1:
             Input: digits = "23"
             Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
